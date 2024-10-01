f1fb2b56bfbe95f9c81b45ed815efed754fac72a
Pete Gadomski <pete.gadomski@gmail.com>
2024-10-01 09:00:07 -0600

    feat: add expected output checks

--- tests/ogc_tests.rs
+++ tests/ogc_tests.rs
@@ -1,17 +1,33 @@
 use assert_json_diff::assert_json_eq;
+use cql2::Expr;
 use rstest::rstest;
 use serde_json::json;
 use std::path::{Path, PathBuf};
 
-fn validate_path(path: impl AsRef<Path>) {
-    let cql2 = std::fs::read_to_string(f).unwrap();
-    let expr = cql2::parse(cql2).unwrap();
+fn validate_str(s: &str) -> Expr {
+    let expr = cql2::parse(s).unwrap();
     assert!(expr.is_valid());
-    let expr = cql2::parse(&expr.to_cql2_text().unwrap()).unwrap();
-    assert!(expr.is_valid());
+    let expr_from_txt = cql2::parse(&expr.to_cql2_text().unwrap()).unwrap();
+    assert!(expr_from_txt.is_valid());
     let json = expr.to_json().unwrap();
-    let expr = cql2::parse(&json).unwrap();
-    assert_json_eq!(json!(json), json!(expr.to_json().unwrap()));
+    let expr_from_json = cql2::parse(&json).unwrap();
+    assert_json_eq!(json!(json), json!(expr_from_json.to_json().unwrap()));
+    expr
+}
+
+fn validate_path(path: impl AsRef<Path>) {
+    let path = path.as_ref();
+    let file_name = path.file_name().unwrap();
+    let expr = validate_str(&std::fs::read_to_string(path).unwrap());
+
+    let input_format = path.parent().unwrap().file_stem().unwrap();
+    let expected = Path::new("tests/expected").join(input_format);
+
+    let json = std::fs::read_to_string(expected.join(file_name).with_extension("json")).unwrap();
+    assert_eq!(json.trim(), expr.to_json().unwrap());
+
+    let text = std::fs::read_to_string(expected.join(file_name).with_extension("txt")).unwrap();
+    assert_eq!(text.trim(), expr.to_cql2_text().unwrap());
 }
 
 #[rstest]
